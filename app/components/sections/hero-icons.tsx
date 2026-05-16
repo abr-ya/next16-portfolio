4f0e618157e375009407aeaac4e1f06935206e3d
abr-ya <gamer05@list.ru>
2026-05-16 19:36:56 +0300

    feat: Add Hero Section content and animation

---
+++ app/components/sections/hero-icons.tsx
@@ -0,0 +1,13 @@
+import InstagramLineIcon from "remixicon-react/InstagramLineIcon";
+import FacebookCircleLineIcon from "remixicon-react/FacebookCircleLineIcon";
+import DribbbleLineIcon from "remixicon-react/DribbbleLineIcon";
+import YoutubeLineIcon from "remixicon-react/YoutubeLineIcon";
+import GithubLineIcon from "remixicon-react/GithubLineIcon";
+
+export const heroIcons = [
+  <InstagramLineIcon />,
+  <FacebookCircleLineIcon />,
+  <DribbbleLineIcon />,
+  <YoutubeLineIcon />,
+  <GithubLineIcon />,
+];
